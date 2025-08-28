ca04779d06d41c518573955e6f730299e0a12588
FOCC <mihocc18@gmail.com>
2025-08-28 05:54:34 +0300

    Quartz sync: Aug 28, 2025, 5:54 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -51,7 +51,15 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(), 
+    Component.ArticleTitle(), 
+    Component.ContentMeta(), 
+    Component.ConditionalRender({
+      component: Component.Graph(),
+      condition: (page) => page.frontmatter?.showGraph === true,
+    })
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
